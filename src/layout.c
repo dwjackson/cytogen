e4c52c656381c9e4af97534f3448b9b5091f30e4
David Jackson <david@dwjackson.ca>
2016-10-08 11:40:33 -0400

    Clean up some code in layout.c

--- src/layout.c
+++ src/layout.c
@@ -19,6 +19,51 @@
 #include "layout.h"
 
 #define LAYOUTS_DIR_NAME "_layouts"
+
+static int
+layouts_count(DIR *layouts_dir)
+{
+    struct dirent *de;
+    int num_layouts = 0;
+    while ((de = readdir(layouts_dir)) != NULL) {
+        char *file_name = de->d_name;
+        if (file_name[0] != '.') {
+            num_layouts++;
+        }
+    }
+    rewinddir(layouts_dir);
+    return num_layouts;
+}
+
+static char
+*layout_name_from_file_name(const char *file_name, size_t file_name_len)
+{
+    char *layout_name = malloc(file_name_len + 1);
+    if (layout_name == NULL) {
+        fprintf(stderr, "ERROR: Could not malloc() for layout_name\n");
+        abort();
+    }
+    memset(layout_name, 0, file_name_len);
+    int i;
+    int ch;
+    for (i = 0; i < file_name_len; i++) {
+        ch = file_name[i];
+        if (ch == '.') {
+            break;
+        }
+        layout_name[i] = ch;
+    }
+    return layout_name;
+}
+
+static char
+*layout_content_read(int fd, size_t file_size)
+{
+    void *region;
+    region = mmap(NULL, file_size, PROT_READ, MAP_SHARED, fd, 0);
+    char *content = (char *)region;
+    return content;
+}
     
 struct layout
 *get_layouts(int *num_layouts_ptr)
@@ -29,17 +74,11 @@ struct layout
     struct dirent *de;
     DIR *layouts_dir = opendir(LAYOUTS_DIR_NAME);
     if (layouts_dir != NULL) {
-        while ((de = readdir(layouts_dir)) != NULL) {
-            char *file_name = de->d_name;
-            if (file_name[0] != '.') {
-                num_layouts++;
-            }
-        }
-        rewinddir(layouts_dir);
-
+        num_layouts = layouts_count(layouts_dir);
+        
         layouts = malloc(sizeof(struct layout) * num_layouts);
         if (layouts == NULL) {
-            fprintf(stderr, "ERROR: Could not malloc()\n");
+            fprintf(stderr, "ERROR: Could not malloc() for layouts\n");
             abort();
         }
 
@@ -48,17 +87,8 @@ struct layout
             char *file_name = de->d_name;
             if (file_name[0] != '.') {
                 size_t file_name_len = strlen(file_name);
-                char *layout_name = malloc(file_name_len + 1);
-                memset(layout_name, 0, file_name_len);
-                int i;
-                int ch;
-                for (i = 0; i < file_name_len; i++) {
-                    ch = file_name[i];
-                    if (ch == '.') {
-                        break;
-                    }
-                    layout_name[i] = ch;
-                }
+                char *layout_name = layout_name_from_file_name(file_name,
+                                                               file_name_len);
                 char *file_path = malloc(strlen(LAYOUTS_DIR_NAME) + 1
                                          + file_name_len
                                          + 1);
@@ -75,10 +105,8 @@ struct layout
                     fprintf(stderr, "ERROR: Could not stat %s\n", file_path);
                     exit(EXIT_FAILURE);
                 }
-                void *r; /* Region */
                 size_t size = statbuf.st_size;
-                r = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
-                char *content = (char *)r;
+                char *content = layout_content_read(fd, size);
                 struct layout l = { layout_name, content, size };
                 layouts[index] = l;
                 close(fd);
@@ -92,6 +120,12 @@ struct layout
     return layouts;
 }
 
+static void
+layout_content_destroy(struct layout layout)
+{
+    munmap(layout.content, layout.length);
+}
+
 void
 layouts_destroy(struct layout *layouts, int num_layouts)
 {
@@ -100,7 +134,7 @@ layouts_destroy(struct layout *layouts, int num_layouts)
     for (i = 0; i < num_layouts; i++) {
         layout = layouts[i];
         free(layout.name);
-        munmap(layout.content, layout.length);
+        layout_content_destroy(layout);
     }
 }
 
