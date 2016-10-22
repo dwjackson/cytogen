b0771c41b1871a64b6b5442a6fe8835683d6effb
David Jackson <david@dwjackson.ca>
2016-10-22 14:35:19 -0400

    Add cyjson files

---
+++ include/cyjson.h
@@ -0,0 +1,47 @@
+
+
+#include <stdbool.h>
+
+enum cyjson_data_type {
+    CYJSON_OBJECT,
+    CYJSON_OBJECT_BEGIN,
+    CYJSON_OBJECT_END,
+
+    CYJSON_ARRAY,
+    CYJSON_ARRAY_BEGIN,
+    CYJSON_ARRAY_END,
+
+    CYJSON_STRING_DELIMITER,
+    CYJSON_STRING,
+
+    CYJSON_NUMBER,
+    CYJSON_BOOLEAN,
+    CYJSON_NULL
+
+    CYJSON_NONE
+};
+
+union cyjson_value {
+    double number;
+    bool boolean;
+    void *null;
+    union cyjson_string {
+        char *buffer;
+        size_t length;
+        size_t bufsize;
+    } string;
+};
+
+struct cyjson_parser {
+    const char *json;
+    enum cyjson_data_type parent_data_type;
+    enum cyjson_data_type token_data_type;
+    union cyjson_value value;
+};
+typedef struct cyjson_parser cyjson_parser_t;
+
+int
+cyjson_parser_init(cyjson_parser_t *parser, const char *json);
+
+int
+cyjson_parse(cyjson_parser_t *parser);
---
+++ lib/cyjson.c
@@ -0,0 +1,32 @@
+#include "cyjson.h"
+
+int
+cyjson_parser_init(cyjson_parser_t *parser, const char *json)
+{
+    parser->json = json;
+    parser->parent_data_type = CYJSON_NONE;
+    parser->token_data_type = CYJSON_NONE;
+    parser->value.null = NULL;
+}
+
+int
+cyjson_parse(cyjson_parser_t *parser)
+{
+    const char *json = parser->json;
+    int ch = *json;
+    if (ch == '{' && parser->parent_data_type != CYJSON_STRING) {
+        parser->token_data_type = CYJSON_OBJECT_BEGIN;
+        parser->parent_data_type = CYJSON_OBJECT;
+    } else if (ch == '}' && parser->parent_data_type != CYJSON_STRING) {
+        parser->token_data_type = CYJSON_OBJECT_END;
+        parser->parent_data_type = CYJSON_NONE;
+    } else if (ch == '[' && parser->parent_data_type != CYJSON_STRING) {
+        parser->token_data_type = CYJSON_ARRAY_BEGIN;
+        parser->parent_data_type = CYJSON_ARRAY;
+    } else if (ch == ']' && parser->parent_data_type != CYJSON_STRING) {
+        parser->token_data_type = CYJSON_ARRAY_END;
+        parser->parent_data_type = CYJSON_NONE;
+    }
+    // TODO
+    (parser->json)++;
+}
