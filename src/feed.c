acb8ab419168a5d748217e74d0d0023107adf305
David Jackson <david@dwjackson.ca>
2016-10-23 13:20:05 -0400

    Add license header to feed.*

--- src/feed.c
+++ src/feed.c
@@ -1,3 +1,13 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2016 David Jackson
+ */
+
 #include "cyto_config.h"
 #include <ctache/ctache.h>
 #include <stdio.h>
