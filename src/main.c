de97c84511216f1c8182c57d9c9b658100d7b260
David Jackson <david@dwjackson.ca>
2017-07-03 18:59:30 -0400

    Better organize main.c

--- src/main.c
+++ src/main.c
@@ -25,7 +25,6 @@
 #include "generate.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -42,121 +41,26 @@
 #define SITE_POSTS_DIR "_site/posts"
 #define MAXFDS 100
 
-/* NOTE: This function is not thread-safe */
+static int
+rename_posts();
+
 static int
 _rename_posts(const char *path,
               const struct stat *stat_ptr,
               int flag,
-              struct FTW *ftw)
-{
-    char *in_path;
-    char *dir;
-    char index_html[] = "index.html";
-    char *out_path;
+              struct FTW *ftw);
 
-    if (flag == FTW_F) {
-        in_path = strdup(path);
-        dir = dirname(in_path);
-        out_path = malloc(strlen(dir) + 1 + strlen(index_html) + 1);
-        strcpy(out_path, dir);
-        strcat(out_path, "/");
-        strcat(out_path, index_html);
-        rename(path, out_path);
-        free(out_path);
-        free(in_path);
-    }
+static void
+cmd_clean();
 
-    return 0;
-}
+static void
+print_help();
 
 static void
 cmd_generate(struct cyto_config *config,
              const char *curr_dir_name,
              const char *site_dir,
-             int num_workers)
-{
-    ctache_data_t *data;
-    pthread_mutex_t data_mutex;
-    struct stat statbuf;
-    bool has_posts;
-    struct generate_arguments args;
-
-    /* Set up the data */
-    data = ctache_data_create_hash();
-    pthread_mutex_init(&data_mutex, NULL);
-
-    /* Set up the posts data */
-    has_posts = stat(POSTS_DIR, &statbuf) == 0 && statbuf.st_mode & S_IFDIR;
-    ctache_data_t *posts_array = NULL;
-    if (has_posts) {
-        posts_array = ctache_data_create_array(0);
-        ctache_data_hash_table_set(data, "posts", posts_array);
-    }
-
-    /* Set up the generation arguments */
-    args.curr_dir_name = curr_dir_name;
-    args.site_dir = site_dir;
-    args.num_workers = num_workers;
-    args.data = data;
-    args.data_mutex = &data_mutex;
-    args.process = process_files;
-
-    /* Perform the generation */
-    if (has_posts) {
-        args.curr_dir_name = POSTS_DIR;
-        args.process = process_post_files;
-
-        generate(&args);
-        nftw(SITE_POSTS_DIR, _rename_posts, MAXFDS, 0);
-
-        args.process = process_files;
-        args.curr_dir_name = curr_dir_name;
-    }
-    generate(&args);
-
-    /* Create the Atom/RSS feed file */
-    if (config != NULL) {
-        generate_feed(config, posts_array);
-    }
-
-    /* Clean up */
-    pthread_mutex_destroy(&data_mutex);
-    ctache_data_destroy(data);
-}
-
-int
-_clean(const char *path, const struct stat *stat, int flag, struct FTW *ftw_ptr)
-{
-    if (flag == FTW_DP) {
-        rmdir(path);
-    } else if (flag == FTW_F) {
-        unlink(path);
-    } else {
-        fprintf(stderr, "Unrecognized file type for %s\n", path);
-        return -1;
-    }
-    return 0;
-}
-
-void
-cmd_clean()
-{
-    nftw(SITE_DIR, _clean, 1000, FTW_DEPTH); 
-}
-
-static void
-print_help()
-{
-    printf("%s\n", USAGE);
-    printf("Flags:\n");
-    printf("\t-h Print this help message\n");
-    printf("\t-V Print the version number\n");
-    printf("\t-j [THREADS] Set number of worker threads (default is 4)\n");
-    printf("Commands:\n");
-    printf("\tgenerate - Generate a site from the current directory\n");
-    printf("\tinit [PROJECT_NAME] - Initialize a cytogen project\n");
-    printf("\tclean - Remove generated site files\n");
-}
+             int num_workers);
 
 int
 main(int argc, char *argv[])
@@ -231,4 +135,126 @@ main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+static int
+rename_posts()
+{
+    return nftw(SITE_POSTS_DIR, _rename_posts, MAXFDS, 0);
+}
+
+/* NOTE: This function is not thread-safe */
+static int
+_rename_posts(const char *path,
+              const struct stat *stat_ptr,
+              int flag,
+              struct FTW *ftw)
+{
+    char *in_path;
+    char *dir;
+    char index_html[] = "index.html";
+    char *out_path;
+
+    if (flag == FTW_F) {
+        in_path = strdup(path);
+        dir = dirname(in_path); /* dirname() is not thread-safe */
+        if (asprintf(&out_path, "%s/%s", dir, index_html) == -1) {
+            fprintf(stderr, "Could not asprintf() index HTML file name\n");
+            abort();
+        }
+        rename(path, out_path);
+        free(out_path);
+        free(in_path);
+    }
+
+    return 0;
+}
+
+static void
+cmd_generate(struct cyto_config *config,
+             const char *curr_dir_name,
+             const char *site_dir,
+             int num_workers)
+{
+    ctache_data_t *data;
+    pthread_mutex_t data_mutex;
+    struct stat statbuf;
+    bool has_posts;
+    struct generate_arguments args;
+
+    /* Set up the data */
+    data = ctache_data_create_hash();
+    pthread_mutex_init(&data_mutex, NULL);
+
+    /* Set up the posts data */
+    has_posts = stat(POSTS_DIR, &statbuf) == 0 && statbuf.st_mode & S_IFDIR;
+    ctache_data_t *posts_array = NULL;
+    if (has_posts) {
+        posts_array = ctache_data_create_array(0);
+        ctache_data_hash_table_set(data, "posts", posts_array);
+    }
+
+    /* Set up the generation arguments */
+    args.curr_dir_name = curr_dir_name;
+    args.site_dir = site_dir;
+    args.num_workers = num_workers;
+    args.data = data;
+    args.data_mutex = &data_mutex;
+    args.process = process_files;
+
+    /* Perform the generation */
+    if (has_posts) {
+        args.curr_dir_name = POSTS_DIR;
+        args.process = process_post_files;
+
+        generate(&args);
+        rename_posts();
+
+        args.process = process_files;
+        args.curr_dir_name = curr_dir_name;
+    }
+    generate(&args);
+
+    /* Create the Atom/RSS feed file */
+    if (config != NULL) {
+        generate_feed(config, posts_array);
+    }
+
+    /* Clean up */
+    pthread_mutex_destroy(&data_mutex);
+    ctache_data_destroy(data);
+}
+
+int
+_clean(const char *path, const struct stat *stat, int flag, struct FTW *ftw_ptr)
+{
+    if (flag == FTW_DP) {
+        rmdir(path);
+    } else if (flag == FTW_F) {
+        unlink(path);
+    } else {
+        fprintf(stderr, "Unrecognized file type for %s\n", path);
+        return -1;
+    }
+    return 0;
+}
+
+static void
+cmd_clean()
+{
+    nftw(SITE_DIR, _clean, 1000, FTW_DEPTH); 
+}
+
+static void
+print_help()
+{
+    printf("%s\n", USAGE);
+    printf("Flags:\n");
+    printf("\t-h Print this help message\n");
+    printf("\t-V Print the version number\n");
+    printf("\t-j [THREADS] Set number of worker threads (default is 4)\n");
+    printf("Commands:\n");
+    printf("\tgenerate - Generate a site from the current directory\n");
+    printf("\tinit [PROJECT_NAME] - Initialize a cytogen project\n");
+    printf("\tclean - Remove generated site files\n");
 }
