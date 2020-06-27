a1a211f06f24e440a4549d533d307b87c945ddf0
David Jackson <dwjackson@users.noreply.github.com>
2020-06-27 10:07:28 -0400

    Add mkheader.sh script to contrib
    This script can be used to create the header for a page without needing
    as much boilerplate.

---
+++ contrib/mkheader.sh
@@ -0,0 +1,17 @@
+## This Source Code Form is subject to the terms of the Mozilla Public
+## License, v. 2.0. If a copy of the MPL was not distributed with this
+## file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+## Copyright (c) 2020 David Jackson
+
+#!/bin/sh
+
+border='---'
+today=`date '+%Y-%m-%d'`
+
+echo "$border"
+echo 'layout: LAYOUT_HERE'
+echo 'title: TITLE_HERE'
+echo 'author: AUTHOR_HERE'
+echo "date: $today"
+echo "$border"
