5119d633ebb84fcd9db19044fc81b4112d0bc0a6
David Jackson <dwjackson@users.noreply.github.com>
2021-05-24 21:13:38 -0400

    Add an expected site directory for test 4

--- test_sites/run_tests.sh
+++ test_sites/run_tests.sh
@@ -1,40 +1,90 @@
 #!/bin/sh
 
 CYTO="../src/cyto"
-SITE_DIR='./_site'
-EXPECTED='./_expected'
+SITE_DIR='_site'
+EXPECTED='_expected'
+
+test_file() {
+	test_name="$1"
+	actual_file="$2"
+	expected_file="$3"
+	d=`diff "$expected_file" "$actual_file" > /dev/null`
+	if [ "$?" -ne 0 ]
+	then
+		printf "FAIL: File is wrong: $actual_file\n"
+		echo 'EXPECTED'
+		cat "$expected_file"
+		echo 'ACTUAL'
+		cat "$f"
+		exit 1
+	fi
+}
+
+test_directory() {
+	test_name="$1"
+	test_dir="$2"
+	for f in `ls "$test_dir"`
+	do
+		actual_file="$test_dir/$f"
+		expected_file=`echo "$actual_file" \
+			| sed -e "s|$SITE_DIR|$EXPECTED|g"`
+
+		if [ ! -f "$actual_file" -a ! -d "$actual_file" ]
+		then
+			printf "FAIL: actual file missing: $actual_file\n"
+			exit 1
+		fi
+
+		if [ -f "$f" -a ! -f "$expected_file" ]
+		then
+			printf "FAIL: Unexpected file: $actual_file\n"
+			echo "EXPECTED: $expected_file"
+			exit 1
+		fi
+
+		if [ -d "$f" -a ! -d "$expected_file" ]
+		then
+			printf "FAIL: Unexpected directory: $actual_file\n"
+			exit 1
+		fi
+
+		if [ -f "$f" ]
+		then
+			test_file "$test_name" "$actual_file" "$expected_file"
+			if [ "$?" -eq 1 ]
+			then
+				exit 1
+			fi
+		elif [ -d "$f" ]
+		then
+			test_directory "$test_name" "$actual_file"
+			if [ "$?" -eq 1 ]
+			then
+				exit 1
+			fi
+		fi
+	done
+}
 
 run_test() {
 	test_name="$1"
+	printf "Running $test_name... "
 	../$CYTO generate
-	for f in `ls "$SITE_DIR"`
-	do
-		if [ ! -d "$EXPECTED" ]
-		then
-			echo "$test_name: ERROR: No $EXPECTED directory"
-			exit 1
-		fi
 
-		expected_file="$EXPECTED/$f"
-		if [ ! -f "$expected_file" ]
-		then
-			echo "$test_name: FAIL: Unexpected file: $f"
-			exit 1
-		fi
+	if [ ! -d "$EXPECTED" ]
+	then
+		echo "$test_name: ERROR: No $EXPECTED directory"
+		exit 1
+	fi
 
-		d=`diff "$expected_file" "$SITE_DIR/$f" > /dev/null`
-		if [ "$?" -ne 0 ]
-		then
-			echo "$test_name: FAIL: File is wrong: $f"
-			echo 'EXPECTED'
-			cat "$expected_file"
-			echo 'ACTUAL'
-			cat "$f"
-			exit 1
-		fi
-	done
+	test_directory "$test_name" "./$SITE_DIR"
+
 	../$CYTO clean
-	echo "$test_name: PASSED"
+
+	if [ "$?" -eq 0 ]
+	then
+		printf "PASS\n"
+	fi
 }
 
 if [ ! -f "$CYTO" ]
